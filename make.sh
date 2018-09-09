b2242960a563801dc960bb186ab9cd7e2c3a18d1
Denis Kasak <dkasak@termina.org.uk>
2018-09-09 11:23:05 +0200

    Specify python2 explicitly.

--- make.sh
+++ make.sh
@@ -6,7 +6,7 @@ set -x
 # TODO: make configurable
 AFL_PATH="$PWD/afl-2.52b"
 
-python rules2code.py < rules/cxx.txt > rules/cxx.hh
+python2 rules2code.py < rules/cxx.txt > rules/cxx.hh
 g++ -std=c++11 -I"${AFL_PATH}" -Wall -g -o main main.cc
 
 mkdir -p output
